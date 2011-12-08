580df2fa011c2cba0c76ef36de60b8097a97c9ca
gvanhove <gregg@slackersoft.net>
2011-12-08 10:25:30 -0800

    remove redundant rspec.rake link database.yml on server so production database credentials aren't committed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'bundler/capistrano'
+
 set :user, "whiteguy013"
 set :domain, "greggandjen.com"
 set :project, "photos"
@@ -18,6 +20,8 @@ set :branch, 'master'
 set :git_shallow_clone, 1
 set :scm_verbose, true
 
+set :use_sudo, false
+
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
 #   task :start do ; end
@@ -26,3 +30,13 @@ set :scm_verbose, true
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+namespace :deploy do
+  namespace :db do
+    task :symlink, :except => { :no_release => true } do
+      run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+    end
+    
+    after "deploy:finalize_update", "deploy:db:symlink"
+  end
+end
