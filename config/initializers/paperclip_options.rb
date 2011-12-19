1b77d6b2ab8b8a580bd4ed43c7212800ccbe2e66
gvanhove <gregg@slackersoft.net>
2011-12-18 19:02:10 -0800

    rename bucket to match newly created bucket

--- config/initializers/paperclip_options.rb
+++ config/initializers/paperclip_options.rb
@@ -1,7 +1,7 @@
 PAPERCLIP_STORAGE_OPTIONS = if Rails.env.production?
    {
     :storage => :s3,
-    :bucket => "photos",
+    :bucket => "gnj-photos",
     :s3_credentials => {
       :access_key_id => ENV['S3_KEY'],
       :secret_access_key => ENV['S3_SECRET']
