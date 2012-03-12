339075b94bb10911836e3f1779f3bfbd6be08ac1
gvanhove <gregg@slackersoft.net>
2012-03-11 18:07:19 -0700

    proper attribute name for login

--- config/initializers/mail_settings.rb
+++ config/initializers/mail_settings.rb
@@ -2,9 +2,9 @@ Mail.defaults do
   if Rails.env.test?
     retriever_method :test
   else
-    retriever_method :pop3,
+    retriever_method :imap,
       address: 'mail.greggandjen.com',
-      username: 'photos@greggandjen.com',
+      user_name: 'photos@greggandjen.com',
       password: "kv*wSc!E" # ENV['EMAIL_PASSWORD']
   end
 end
