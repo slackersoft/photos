57d59b54c2289afccd1160cfcb57c70842d1ee68
gvanhove <gregg@slackersoft.net>
2012-03-11 18:47:52 -0700

    add testing aws config for dev

--- config/initializers/paperclip_options.rb
+++ config/initializers/paperclip_options.rb
@@ -7,6 +7,15 @@ PAPERCLIP_STORAGE_OPTIONS = if Rails.env.production?
       :secret_access_key => ENV['S3_SECRET']
     }
   }
+#elsif Rails.env.development?
+#  {
+#    :storage => :s3,
+#    :bucket => "gnj-photos-dev",
+#    :s3_credentials => {
+#      :access_key_id => ENV['S3_KEY'],
+#      :secret_access_key => ENV['S3_SECRET']
+#    }
+#  }
 else
   {
     :url => "/system/:attachment/:id/:style.:extension"
