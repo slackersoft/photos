0c31cba89e12dc7c651b52ff327b09829717f368
gvanhove <gregg@slackersoft.net>
2012-04-07 16:06:45 -0700

    use pop3 instead of IMAP for better deleting

--- config/initializers/mail_settings.rb
+++ config/initializers/mail_settings.rb
@@ -2,7 +2,7 @@ Mail.defaults do
   if Rails.env.test?
     retriever_method :test
   else
-    retriever_method :imap,
+    retriever_method :pop3,
       address: 'mail.greggandjen.com',
       user_name: 'photos@greggandjen.com',
       password: ENV['EMAIL_PASSWORD']
